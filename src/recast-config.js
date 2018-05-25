55166e06e0c3fdce58b7a5f4f0b6c2e033ee69da
Don McCurdy <don.r.mccurdy@gmail.com>
2018-05-24 22:19:14 -0700

    Add tooltips.

--- src/recast-config.js
+++ src/recast-config.js
@@ -1,10 +1,70 @@
 const RecastConfig = [
-  {name: 'cellSize', default: 0.3, min: 0.1, max: 3, step: 0.01 },
-  {name: 'cellHeight', default: 0.2, min: 0.1, max: 3, step: 0.01 },
-  {name: 'agentHeight', default: 0.8, min: 0.1, max: 3, step: 0.01 },
-  {name: 'agentRadius', default: 0.2, min: 0.1, max: 3, step: 0.01 },
-  {name: 'agentMaxClimb', default: 0.5, min: 0.1, max: 5, step: 0.01 },
-  {name: 'agentMaxSlope', default: 30, min: 0, max: 90, step: 1 }
+  {
+    name: 'cellSize',
+    default: 0.3,
+    min: 0.1,
+    max: 3,
+    step: 0.01,
+    description: 'Width/depth of voxel cells used to sample scene geometry.'
+  },
+  {
+    name: 'cellHeight',
+    default: 0.2,
+    min: 0.1,
+    max: 3,
+    step: 0.01,
+    description: 'Height of voxel cells used to sample scene geometry.'
+  },
+  {
+    name: 'agentHeight',
+    default: 0.8,
+    min: 0.1,
+    max: 3,
+    step: 0.01,
+    description:
+`Minimum floor to 'ceiling' height that will still allow the floor
+area to be considered walkable. Permits detection of overhangs in
+the source geometry that make the geometry below un-walkable. The
+value is usually set to the maximum agent height.`
+  },
+  {
+    name: 'agentRadius',
+    default: 0.2,
+    min: 0.1,
+    max: 3,
+    step: 0.01,
+    description:
+`The distance to erode/shrink the walkable area of the heightfield
+away from obstructions. In general, this is the closest any part
+of the final mesh should get to an obstruction in the source geometry.
+It is usually set to the maximum agent radius. Areas too narrow will
+be considered "blocked."`
+  },
+  {
+    name: 'agentMaxClimb',
+    default: 0.5,
+    min: 0.1,
+    max: 5,
+    step: 0.01,
+    description:
+`Maximum ledge height that is considered to still be traversable.
+Allows the mesh to flow over low lying obstructions such as curbs
+and up/down stairways. The value is usually set to how far up/down
+an agent can step.`
+  },
+  {
+    name: 'agentMaxSlope',
+    default: 30,
+    min: 0,
+    max: 90,
+    step: 1,
+    description: 'The maximum slope that is considered walkable.'
+  }
 ];
+
+// Remove line returns.
+RecastConfig.map((param) => {
+  param.description = param.description.replace(/\n/g, ' ');
+});
 
 module.exports = RecastConfig;
